4864adb90e74b2f55a67538aef64cf70e92eecfc
arushdhariwal <dhariwalarush971@gmail.com>
2025-07-10 20:03:01 +0530

    Create Task2.py

---
+++ Task2.py
@@ -0,0 +1,23 @@
+def calculator():
+    print("Simple Calculator")
+    print("Operations: +, -, *, /")
+
+    a = float(input("Enter first number: "))
+    op = input("Enter operator: ")
+    b = float(input("Enter second number: "))
+
+    if op == '+':
+        print("Result:", a + b)
+    elif op == '-':
+        print("Result:", a - b)
+    elif op == '*':
+        print("Result:", a * b)
+    elif op == '/':
+        if b != 0:
+            print("Result:", a / b)
+        else:
+            print("Error: Division by zero")
+    else:
+        print("Invalid operator")
+
+calculator()
